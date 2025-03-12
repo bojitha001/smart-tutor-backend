ed1b64af23c31e2099845713f5821eb69a7e762e
lashan <ravijalashangunawardena@gmail.com>
2025-03-12 13:12:30 +0530

    Authentication backend started

---
+++ src/api/middleware/authorization-middleware.ts
@@ -0,0 +1,15 @@
+import ForbiddenError from "../../domain/errors/forbidden-error";
+import { NextFunction, Request, Response } from "express";
+
+const AuthorizationMiddleware = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+    ) => {
+        if(req.auth.claims.metadata.role !== "admin"){
+            throw new ForbiddenError("Admin only route");
+        }
+        next();
+    };
+
+export default AuthorizationMiddleware;
