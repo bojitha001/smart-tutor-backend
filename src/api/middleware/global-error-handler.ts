3667b54f0af101bb3adcd966e7f8256dea955889
lashan <ravijalashangunawardena@gmail.com>
2025-03-12 13:24:17 +0530

    middleware and error implementation

--- src/api/middleware/global-error-handler.ts
+++ src/api/middleware/global-error-handler.ts
@@ -1,12 +1,36 @@
-import { NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
 
 const GlobalErrorHandlingMiddleware = (
+  error: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-      console.log("OOPS");
-      next();
+      // console.log("OOPS");
+      // next();
+      switch(error.name){
+        case "NotFoundError":
+          console.log(error)
+          return res.status(404).json({message: error.message.replaceAll("\n","")});
+          break;
+        case "validationError":
+          console.log(error)
+          return res.status(400).json({message: error.message.replaceAll("\n","")});
+          break;
+        case "Unauthenticated":
+          console.log(error)
+          return res.status(401).json({message: error.message.replaceAll("\n","")});
+          break;
+        case "Error":
+          if(error.message === "Unauthenticated"){
+            console.log(error)
+            return res.status(401).json({message: error.message.replaceAll("\n","")});
+        }
+        default:
+          console.log(error)
+          return res.status(500).json({message: error.message});
+          break;
+      }
 };
 
 export default GlobalErrorHandlingMiddleware;
