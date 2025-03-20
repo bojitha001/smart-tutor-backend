8f9a3a35a708c0b4b7526f9cd34fd8dc620266a2
lashan <ravijalashangunawardena@gmail.com>
2025-03-20 18:51:22 +0530

    restricted access to logged in customers for kuppi groups

--- src/api/communities.ts
+++ src/api/communities.ts
@@ -1,9 +1,10 @@
 import express from "express"
 import { addCommunity, getAllCommunities, getCommunityById } from "../application/communities";
+import { requireAuth } from "@clerk/express";
 
 const communityRouter = express.Router();
 
-communityRouter.route('/').get(getAllCommunities).post(addCommunity);
-communityRouter.route('/:id').get(getCommunityById);
+communityRouter.route('/').get(requireAuth(), getAllCommunities).post(requireAuth(), addCommunity);
+communityRouter.route('/:id').get(requireAuth(), getCommunityById);
 
 export default communityRouter;
--- src/api/communityQuestionFrom.ts
+++ src/api/communityQuestionFrom.ts
@@ -1,9 +1,11 @@
 import express from "express"
 import { createCommunityQuestionForm, getCommunityQuestionForm, getCommunityQuestionFormById } from "../application/communityQuestionFrom";
+import { requireAuth } from "@clerk/express";
+// import AuthorizationMiddleware from "./middleware/authorization-middleware";
 
 const communityQuestionFormRouter = express.Router();
 
-communityQuestionFormRouter.route('/').get(getCommunityQuestionForm).post(createCommunityQuestionForm);
-communityQuestionFormRouter.route('/:id').get(getCommunityQuestionFormById);
+communityQuestionFormRouter.route('/').get(requireAuth(), getCommunityQuestionForm).post(requireAuth(), createCommunityQuestionForm);
+communityQuestionFormRouter.route('/:id').get(requireAuth(), getCommunityQuestionFormById);
 
 export default communityQuestionFormRouter;
