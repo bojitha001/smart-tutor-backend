8b14a55c0eed848b51f98f4f279938d8289d02b9
lashan <ravijalashangunawardena@gmail.com>
2025-03-20 23:27:24 +0530

    settingup the publicmetadata role

--- src/api/communityQuestionFrom.ts
+++ src/api/communityQuestionFrom.ts
@@ -1,11 +1,11 @@
 import express from "express"
 import { createCommunityQuestionForm, getCommunityQuestionForm, getCommunityQuestionFormById } from "../application/communityQuestionFrom";
 import { requireAuth } from "@clerk/express";
-// import AuthorizationMiddleware from "./middleware/authorization-middleware";
+import AuthorizationMiddleware from "./middleware/authorization-middleware";
 
 const communityQuestionFormRouter = express.Router();
 
-communityQuestionFormRouter.route('/').get(getCommunityQuestionForm).post(requireAuth(), createCommunityQuestionForm);
+communityQuestionFormRouter.route('/').get(getCommunityQuestionForm).post(requireAuth(), AuthorizationMiddleware, createCommunityQuestionForm);
 communityQuestionFormRouter.route('/:id').get(getCommunityQuestionFormById);
 
 export default communityQuestionFormRouter;
