631aeb0263550cfdf69147770a05662c457e43b9
lashan <ravijalashangunawardena@gmail.com>
2025-03-23 22:39:05 +0530

    url updated

--- src/index.ts
+++ src/index.ts
@@ -35,4 +35,4 @@ app.use('/bookings', bookingsRouter);
 app.use('/chatbot', chatbotRouter);
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server is listening on port${PORT}.`));
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}.`));
