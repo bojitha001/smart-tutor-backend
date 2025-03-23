5be38b14e58b61baa88dd7c2d5d1098a6e14908a
lashan <ravijalashangunawardena@gmail.com>
2025-03-23 21:18:04 +0530

    fixed dynamic port

--- src/index.ts
+++ src/index.ts
@@ -34,5 +34,5 @@ app.use('/students', studentRouter);
 app.use('/bookings', bookingsRouter);
 app.use('/chatbot', chatbotRouter);
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is listening on port${PORT}.`));
